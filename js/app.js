4e11754aafe190a3be01ef25d9fccd14dc338cd0
Stephanie Hiyagon <mv.hiyagon@gmail.com>
2017-08-17 02:33:46 -0500

    formulario

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log('app.js');
 $('ul.nav-menu-inner').hide();
+$('.oculto').hide();
 
 function moveScroll(event){
   var scroll=window.pageYOffset || document.body.scrollTop;
@@ -22,3 +23,17 @@ $('.item-buho a').on("click", function(e) {
     $('html,body').animate({scrollTop: $(target).offset().top},1200);
     return false;
    });
+
+//se parte
+
+$('.dropdown-menu').on('click',function(e){
+  e.preventDefault();
+  console.log($(e.target).attr('role-client'));
+  if($(e.target).attr('role-client')=='profesor'){
+    console.log('mostrar');
+    $('.oculto').fadeIn('slow');
+  }else{
+    $('.oculto').fadeOut('slow');
+  }
+
+})
